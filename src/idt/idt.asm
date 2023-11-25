a3b53f1f6498ada937fb83095fdc8e370bea0a9e
gcarvellas <gcarvel264@gmail.com>
2023-11-25 01:48:47 -0500

    GDT is loaded but after a second qemu crashes

--- src/idt/idt.asm
+++ src/idt/idt.asm
@@ -7,21 +7,13 @@ global no_interrupt
 %include "./src/utils/macros.asm"
 
 no_interrupt:
-    cli
     pushaq
-    mov rdi, rsp
     call no_interrupt_handler
     popaq
-    add rsp, 24
-    sti
     iretq
 
 int20h:
-    cli
     pushaq
-    mov rdi, rsp
     call int20h_handler
     popaq
-    add rsp, 24
-    sti
     iretq
